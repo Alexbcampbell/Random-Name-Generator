9f08d2fad6e703154f1df7afd34ab09ad841f5b0
Alex Campbell <alexbe.campbell@gmail.com>
2021-01-28 16:52:56 -0600

    GET and POST sagas added

--- src/redux/sagas/raffle.saga.js
+++ src/redux/sagas/raffle.saga.js
@@ -10,8 +10,23 @@ function* postRaffleEntry(action) {
   }
 }
 
-function* raffleSaga() {
-  yield takeLatest('POST_RAFFLE_ENTRY', postRaffleEntry);
+function* getRaffleEntry(action) {
+  try {
+    const response = yield axios.get(`/api/raffle`);
+    yield put({
+      type: 'SET_RAFFLE_ENTRY',
+      payload: response.data,
+    });
+    console.log('look HERE get raffle entries', response.data);
+  } catch (err) {
+    console.log('error getting raffle entries', err);
+    yield put({ type: 'GET_RAFFLE_ENTRY_FAILED' });
+  }
 }
 
-export default registrationSaga;
+function* raffleSaga() {
+  yield takeLatest('POST_RAFFLE_ENTRY', postRaffleEntry);
+  yield takeLatest('GET_RAFFLE_ENTRY', getRaffleEntry);
+}
+
+export default raffleSaga;
